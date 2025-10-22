ede2ae8c057071c613c34a755f9b61712f86464e
prxfsk17 <yakubsha22888@gmail.com>
2025-10-22 18:57:59 +0300

    Tasks from day 13 completed

---
+++ Day 13/Describe the problem.py
@@ -0,0 +1,11 @@
+def my_function():
+    for i in range(1, 20): #21
+        if i==20:
+            print("You got it")
+
+my_function()
+
+# Describe the Problem - Write your answers as comments
+# What is the for loop doing? assign i value from 1 to 19
+# When is the function meant to print 'You got it'? when i get the value of 20
+# What are your assumptions about the value of i? it won't become 20
---
+++ Day 13/Print is Your Friend.py
@@ -0,0 +1,8 @@
+word_per_page = 0
+pages = int(input())
+# print(word_per_page == int(input()))
+word_per_page = int(input())
+total_words = pages*word_per_page
+# print(pages)
+# print(word_per_page)
+print(total_words)
