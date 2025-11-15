4118b27a66d7c26df59e7c607d4cd4fe5f5d04e6
prxfsk17 <yakubsha22888@gmail.com>
2025-11-16 00:45:27 +0300

    Tasks from day 39 continuing

--- Day 39/data_manager.py
+++ Day 39/data_manager.py
@@ -1,3 +1,46 @@
+import os
+import requests
+from dotenv import load_dotenv
+from flight_search import FlightSearch
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self, fs):
+        load_dotenv()
+        self.API_SHEET_URL_GET=os.getenv("API_SHEET_URL")
+        self.API_SHEET_URL_PUT=self.API_SHEET_URL_GET+"/"
+        self.API_SHEET_TOKEN=os.getenv("API_SHEET_TOKEN")
+        self.HEADER={
+            "Authorization" : os.getenv("API_SHEET_TOKEN")
+        }
+        self.flight_search=fs
+        self.records = self.get_sheet()
+        self.update_sheet()
+
+    def get_sheet(self):
+        response = requests.get(url=self.API_SHEET_URL_GET, headers=self.HEADER)
+        response.raise_for_status()
+        return response.json()
+
+    def update_iata_in_record(self):
+        for city in self.records["prices"]:
+            city["iataCode"] = self.flight_search.get_iata(city_name=city["city"])
+
+    def update_sheet(self):
+        self.update_iata_in_record()
+        for city in self.records["prices"]:
+            json = {
+                "price": {
+                    "city": city["city"].title(),
+                    "iataCode": city["iataCode"].title(),
+                    "lowestPrice": city["lowestPrice"],
+                    "id": city["id"]
+                }
+            }
+            response = requests.put(url=self.API_SHEET_URL_PUT + f"{city["id"]}", headers=self.HEADER, json=json)
+            response.raise_for_status()
+            print(response.text)
+
+    def get_record(self):
+        return self.records["prices"]
--- Day 39/flight_data.py
+++ Day 39/flight_data.py
@@ -1,3 +1,67 @@
+import datetime
+import os
+from dotenv import load_dotenv
+import requests
+from flight_search import FlightSearch
+
+def get_cheapest_among_flights(flights):
+    if len(flights) > 0:
+        returned_flight = flights[0]
+        min_price = flights[0]["price"]["grandTotal"]
+        for flight in flights:
+            if not flight is None and flight["price"]["grandTotal"] < min_price:
+                min_price = flight["price"]["grandTotal"]
+                returned_flight = flight
+        return returned_flight
+    else:
+        return None
+
+
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+
+    def __init__(self, arrival_code, arrival, fs):
+        load_dotenv()
+        self.price = 0
+        self.departure_airport_code=os.getenv("AIR_CODE_DEPARTURE")
+        self.arrival_airport_code=arrival_code
+        self.departure_name="Warsaw"
+        self.arrival_name=arrival
+        self.URL=os.getenv("FLIGHT_API_URL")
+        self.flight = None
+        self.flight_search = fs
+
+    def get_cheapest_on_interval(self):
+        print(f"Getting flights for {self.arrival_name}...")
+        today = datetime.datetime.now() + datetime.timedelta(days=1)
+        flights = [self.get_cheapest_flight_on_day(today.strftime("%Y-%m-%d"))]
+        for i in range(0, 170):
+            tomorrow = today + datetime.timedelta(days=1)
+            tomorrow_flight = self.get_cheapest_flight_on_day(tomorrow.strftime("%Y-%m-%d"))
+            flights.append(tomorrow_flight)
+            today = tomorrow
+            print(tomorrow.strftime("%Y-%m-%d"), flights[i]["price"]["grandTotal"])
+        self.flight = get_cheapest_among_flights(flights)
+        print(f"{self.arrival_name}: {self.flight["price"]["grandTotal"]} EUR.")
+
+    def get_cheapest_flight_on_day(self, date):
+        parameters = {
+            "originLocationCode": self.departure_airport_code,
+            "destinationLocationCode": self.arrival_airport_code,
+            "departureDate": date,
+            "adults": 1,
+            "travelClass": "ECONOMY",
+            "nonStop": "true",
+            "currencyCode": "EUR"
+        }
+        head = {
+            "Authorization": f"Bearer {self.flight_search.get_token()}"
+        }
+        response = requests.get(url=self.URL, headers=head, params=parameters)
+        response.raise_for_status()
+        try:
+            flights = response.json()["data"]
+        except KeyError:
+            return None
+        return get_cheapest_among_flights(flights)
+
